5cfbaa2ae2a87d53fee249a4f974960f8c1d4420
lisandro52 <lisandro52@hotmail.com>
2015-05-04 05:33:47 -0300

    Initial commit
    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# angular-routing-app
+
+
---
+++ server.js
@@ -0,0 +1,17 @@
+﻿//get the needed libraries
+var express = require('express');
+var app = express();
+var path = require('path');
+
+//set the public folder to serve public assets
+app.use(express.static(__dirname + '/public'));
+
+//set up our one route to the index.html file
+app.get('*', function (req, res) {
+    console.log('Incoming connection');
+    res.sendFile(path.join(__dirname + '/public/views/index.html'));
+});
+
+//start the server on port 8080
+app.listen(8080);
+console.log('Connect to port 8080!');
